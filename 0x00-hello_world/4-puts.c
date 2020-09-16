e52c220242bc9126b5c64f0f510d991c95a4c88a
constan1000 <hrairimejdi99@gmail.com>
2020-09-16 13:16:48 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - Prints string
- * Description: Prints "\Programming is like building a multilingual puzzle"
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilinngual puzzle");
-  return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
