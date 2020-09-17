d9194364150a07ba10be4167522872b20f1e0d5a
constan1000 <hrairimejdi99@gmail.com>
2020-09-18 00:46:04 +0100

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - prints out all numbers under 10
+ * main - prints out all the numbers under 10 with a ,
  * Return: 0
  */
 int main(void)
 {
-int i;
-for (i = 48; i < 59; i++)
-putchar ("%i");
-putchar (',');
-putchar (' ');
-return (0);
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
