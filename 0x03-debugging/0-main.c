ac87b24ff30e82fa57c9be95097bce87c1ebc2c3
constan1000 <hrairimejdi99@gmail.com>
2020-09-22 10:21:26 +0100

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,23 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "holberton.h"
+
 /**
- * main - generates random number and displays whether number
- * is positive, zero, or negative
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int n;
+        int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n == 0)
-		printf("%i is zero\n", n);
-	else
-		printf("%i is negative\n", n);
-	return (0);
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
 }
