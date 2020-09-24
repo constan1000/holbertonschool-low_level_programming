fa750f56a9184bf6181ae23cd405815d4ae781ae
constan1000 <hrairimejdi99@gmail.com>
2020-09-24 13:10:44 +0100

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+  return (c >= 48 && c< 58);
+}
