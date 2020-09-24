bd7ed3e616cbdccc28f67e4c43d581f80353327e
constan1000 <hrairimejdi99@gmail.com>
2020-09-24 16:04:43 +0100

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,26 +4,17 @@
  * @n: integer to test
  * Return: returns n if positive, 0 if zero, -n if negative
  */
-int _abs(int n)
+int print_last_digit(int n)
 {
 	if (n > 0)
+	  (n = n % 10)
 		return (n);
 	else if (n == 0)
 		return (0);
 	else
-		return (-n);
+	  (n = n % 10 );
+	  return (-n);
+
 }
 
-/**
- * print_last_digit - prints last digit of n
- * @n: integer to be tested
- * Return: last digit of n
- */
-int print_last_digit(int n)
-{
-	int lastDigit;
 
-	lastDigit = _abs((n % 10));
-	_putchar(lastDigit + 48);
-	return (lastDigit);
-}
