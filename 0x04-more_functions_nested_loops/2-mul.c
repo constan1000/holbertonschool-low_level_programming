64f8c953a7045760f50e420db6732f9acc3fd305
constan1000 <hrairimejdi99@gmail.com>
2020-09-24 13:39:04 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code for Holberton School students.
+ *@a: first numb
+ *@b: secand numb
+ * Return: product a*b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
