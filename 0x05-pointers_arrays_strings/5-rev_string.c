b62d6383048712cc0139898e7499d0f1b49f7662
constan1000 <hrairimejdi99@gmail.com>
2020-09-29 11:36:03 +0100

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+char *p = s;
+while (*s)
+s++;
+return (s - p);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: string s
+ */
+void rev_string(char *s)
+{
+int size = _strlen(s);
+char tmp;
+char *beg = s, *end = s;
+end += size - 1;
+while (beg < end)
+{
+tmp = *beg;
+*beg = *end;
+*end = tmp;
+beg++;
+end--;
+}
+}
