9e92f172c3f7bf5d1ebf45300e5d9c3133441d06
constan1000 <hrairimejdi99@gmail.com>
2020-10-02 10:50:45 +0100

    fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,12 @@
 #include "holberton.h"
 
 /**
- * _strlen - swaps the value of two integers
+ *_strlen - swaps the value of two integers
  *@s: pointer s
  */
 int _strlen(char *s)
 {
 int i;
-
 for (i = 0; *s != '\0'; i++)
 {
 s++;
